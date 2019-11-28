e168f7e2c74a7426477cb0286b9e529643fbe800
paulodhiambo <44492906+paulodhiambo@users.noreply.github.com>
2019-11-28 16:44:46 +0300

    portfolio

---
+++ src/components/timeline.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Freelance Developer <span>2019-present</span></h2>
+                        <p>I am a freelance developer majoring in Android and web Backend development.
+                          I am also exploring the role of container technology like Docker and container-orchrastation
+                          technologies like K8s, Swarm to create industry standard production environments.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduate at JKUAT<span>2018-2022</span></h2>
+                        <p>I am pursuing my under-graduate studies with major in Computer Science.
+                          I have taken courses like DSA, OOPs, Computer Networks,Web-Data Management
+                          over the years and have better understanding of these subjects.I have also
+                          been part of S.D.G.(Student Developer Group).</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2005-2017</span></h2>
+                        <p>I have completed my higher secondary education with a mean grade of B plain.
+                          During my time at school, I have developed interest in solving complex problems
+                          of the fundamental physics which helped me to improve my understanding of any problem
+                          and also my mathematical skills to actually solve the problems.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
