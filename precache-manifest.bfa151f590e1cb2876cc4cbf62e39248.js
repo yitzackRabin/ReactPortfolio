2f7f149d72c800b95b1b9278be51bc5564502699
paulodhiambo <44492906+paulodhiambo@users.noreply.github.com>
2019-11-28 17:15:37 +0300

    Updates

---
+++ precache-manifest.bfa151f590e1cb2876cc4cbf62e39248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d556c2336509d609a547f20036282e60",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "a452715180903623347f",
+    "url": "/ReactPortfolio/static/css/main.92237496.chunk.css"
+  },
+  {
+    "revision": "a8a40e74217288c4bf8b",
+    "url": "/ReactPortfolio/static/js/2.ded1e34a.chunk.js"
+  },
+  {
+    "revision": "a452715180903623347f",
+    "url": "/ReactPortfolio/static/js/main.d8e125cb.chunk.js"
+  },
+  {
+    "revision": "91281709c5a64e55da41",
+    "url": "/ReactPortfolio/static/js/runtime-main.4d6b1fb5.js"
+  }
+]);
