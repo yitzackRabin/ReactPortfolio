ddb3e87fd083a75d1e7d0c753ab2d516c16b2977
paulodhiambo <44492906+paulodhiambo@users.noreply.github.com>
2019-11-28 18:44:47 +0300

    text

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -45,8 +45,24 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
+                          <h1>I love Sharing<br /> KNOWLEDGE !!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://great-stegosaurus-fdca3.netlify.com/" target="_blank" rel="noopener noreferrer">View blog <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li style={{backgroundImage: 'url(images/img_bg.png)',  backgroundPosition:"center",  backgroundSize:"contain"}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
                           <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/paulodhiambo" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/paulodhiambo" target="_blank" rel="noopener noreferrer">View posts <i className="icon-book" /></a></p>
                         </div>
                       </div>
                     </div>
